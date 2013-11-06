42ea09a4b62a82c2cb3d358211151648500f04d3
sidan5 <sidan5@gmail.com>
2013-11-06 15:07:52 +0200

    v1.4.4 - Fix issue with session_id

--- EvaTest/EvaFramework/Eva.framework/Versions/A/Headers/Eva.h
+++ EvaTest/EvaFramework/Eva.framework/Versions/A/Headers/Eva.h
@@ -4,14 +4,13 @@
 //
 //  Created by idan S on 5/12/13.
 //  Copyright (c) 2013 Evature. All rights reserved.
-//
+//  Version 1.4.4
 //
 
 #import <Foundation/Foundation.h>
 #import <AVFoundation/AVFoundation.h>
 #import <CoreLocation/CoreLocation.h>
 
-#define EVA_FRAMEWORK_VERSION @"v1.4.4"
    
 @protocol EvaDelegate <NSObject>
 @optional
