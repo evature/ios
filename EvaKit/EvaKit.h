7f6a52c64058fb356515fa5514e97a384b8b74e5
Yehor Popovych <y.popovych@crossplusplus.com>
2015-07-02 17:15:59 +0300

    Initial Commit

---
+++ EvaKit/EvaKit.h
@@ -0,0 +1,19 @@
+//
+//  EvaKit.h
+//  EvaKit
+//
+//  Created by Yegor Popovych on 7/2/15.
+//  Copyright (c) 2015 Evature. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EvaKit.
+FOUNDATION_EXPORT double EvaKitVersionNumber;
+
+//! Project version string for EvaKit.
+FOUNDATION_EXPORT const unsigned char EvaKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EvaKit/PublicHeader.h>
+
+
