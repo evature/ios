b9f803949ba3db06d7def4e7810bae9e3152d8fd
Iftah Haimovitch <iftahh@gmail.com>
2016-02-15 17:03:00 +0200

    v 2.0.16 - added create-meeting and open-map delegates

--- EvaKit/EVApplication.h
+++ EvaKit/EVApplication.h
@@ -18,7 +18,7 @@
 #define EV_NEW_SESSION_ID @"1"
 #define EV_DEFAULT_MAX_RECORDING_TIME 15.0f
 
-#define EV_KIT_VERSION @"2.0.15"
+#define EV_KIT_VERSION @"2.0.16"
 
 // [[NSBundle bundleForClass:[EVApplication class]] objectForInfoDictionaryKey:@"CFBundleShortVersionString"]
 
