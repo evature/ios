87cd897f4f5d44db6a735e035605113bc0c9340f
Iftah Haimovitch <iftahh@gmail.com>
2016-03-14 16:30:29 +0200

    added placeholder credentials.h file for the ExampleApp

---
+++ EvaKitExample/credentials.h
@@ -0,0 +1,19 @@
+//
+//  credentials.h
+//  EvaKit
+//
+//  Created by Iftah Haimovitch on 14/03/2016.
+//  Copyright © 2016 Evature. All rights reserved.
+//
+
+#ifndef credentials_h
+#define credentials_h
+
+// register at http://www.evature.com/registration/form to get your credentials
+#define API_KEY        @"Your_API_KEY_here"
+#define SITE_CODE      @"Your_SITE_CODE_here
+
+// scope should be bit OR of the EVSearchContextType enums - whatever your App supports
+#define SCOPE  EVSearchContextTypeFlight
+
+#endif /* credentials_h */
